ee18e33d528b8dfaab737e8795597c76462b6db1
Anna <apelsann@ya.ru>
2017-01-16 19:16:00 +0300

    jade

---
+++ build/js/main.js
@@ -0,0 +1,1 @@
+$(".calculation__open").on("click",function(){$(this).toggleClass("calculation__open--open"),$(this).parents(".calculation__item").find(".calculation__content").slideToggle("fast")});
